2a794f39a920edc40267e5b5159270c80fb83cb5
AntonioSanBer <antonio.asb94@gmail.com>
2022-11-15 23:31:16 +0100

    Leap year exercise

---
+++ leap_year.py
@@ -0,0 +1,8 @@
+def year(año):
+    print ("el año introducido es",año)
+    if año %4 == 0:
+        print ("el año", año, "es bisiesto")
+    else:
+        print(año, "no es un año bisiesto")
+
+year (2022)
